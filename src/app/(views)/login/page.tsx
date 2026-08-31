e405dccacf97c351922d7cd8c05f37366c686233
Denis Gukov <lgzn151892@gmail.com>
2026-08-31 17:57:47 +0300

    main-page: started main page

---
+++ src/app/(views)/login/page.tsx
@@ -0,0 +1,9 @@
+const LoginPage = () => {
+  return (
+    <div className="flex flex-col items-center justify-center min-h-screen bg-gray-100">
+      <h1 className="text-4xl font-bold mb-8">Login</h1>
+      </div>
+  );
+}
+
+export default LoginPage;
